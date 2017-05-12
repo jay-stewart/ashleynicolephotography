559175ac0dc950a1395545a54db921da7ed38bdd
jay-stewart <jay_stewart10@hotmail.com>
2017-05-12 13:22:45 -0500

    Update page.php

--- page.php
+++ page.php
@@ -1,19 +1,16 @@
 <?php get_header(); ?>
+	<div class="container">
+		<div id="primary" class="content-area"> 
+			<main id="main" class="site-main" role="main">
+   				<?php   while ( have_posts() ) : the_post();
+    					get_template_part( 'template-parts/page/content', 'page' );
+    					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :     
+						comments_template();   
+					endif;
+   				endwhile; // End of the loop.   ?>
+  			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .container -->
+<?php get_footer();
 
-	<?php if ( have_posts() ) : ?
-
-                <?php while ( have_posts() ) : the_post(); ?>
-                        <?php the_content(); ?>
-                <?php endwhile; ?>
-
-            <?php else : ?>
-                <section class="content">
-                    <div class="row">
-                        <?php get_template_part( 'content', 'none' ); ?>
-                    </div>
-                </section>
-            <?php endif; ?>
-
-    <?php //endif; ?>
-
-<?php get_footer(); ?>
