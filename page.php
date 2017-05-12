0283d2c0c15f0767c85ec4a48b51fdd8bf8aa389
jay-stewart <jay_stewart10@hotmail.com>
2017-05-12 13:13:35 -0500

    Update page.php

--- page.php
+++ page.php
@@ -1,29 +1,13 @@
 <?php get_header(); ?>
 
-
-<?php /* if ( !is_home() && is_front_page ()) : ?>
-
-		<div id="backstretch">
-		<?php $images = rwmb_meta( 'anp_imgadv' );
-			if ( !empty( $images ) ) {
-				foreach ( $images as $image ) {
-					echo "<img src='{$image['url']}' alt='{$image['alt']}' width='{$image['width']}' height='{$image['height']}' srcset='{$image['srcset']}' />";
-				}
-			}
-
-		?>
-		</div>
-    <?php else: */ ?>
-	
-
-            <?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?
 
                 <?php while ( have_posts() ) : the_post(); ?>
                         <?php the_content(); ?>
                 <?php endwhile; ?>
 
             <?php else : ?>
-                <section class="content container">
+                <section class="content">
                     <div class="row">
                         <?php get_template_part( 'content', 'none' ); ?>
                     </div>
@@ -31,16 +15,5 @@
             <?php endif; ?>
 
     <?php //endif; ?>
-
-<?php if ( !is_home() && is_front_page ()) : ?>
-
-	<section class="testimonials container">
-		<div class="row">
-			<h5 class="textcenter">Some kind words from past clients:</h5>
-			<?php get_template_part('template-parts/content', 'testimonial'); ?>
-		</div>
-	</section>
-	
-<?php  endif; ?>	
 
 <?php get_footer(); ?>
